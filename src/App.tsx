d8acf952df92c413b00c48f5bd548a4bdc04d30f
kenunezm <kenunezm@gmail.com>
2025-03-26 19:07:51 -0300

    Fix favicon and update title for the browser tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "./components/Header";
 import SummonerSection from "./components/SummonerSection";
 import SummonerSectionMobile from "./components/SummonerSectionMobile";
